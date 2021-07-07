ae0b8bffc6fb75be920bd1fe292a95efae456bbd
Inte-H <sjobs66@gmail.com>
2021-07-07 22:36:54 +0900

    모델, 시간기능 생성

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title=models.CharField(max_length=50)
+    content=models.TextField()
+    post_date=models.DateTimeField(auto_now_add=True)
+    update_date=models.DateTimeField(auto_now=True)
+    # author 나중에
+
+    def __str__(self):
+        return f'{self.title}'
