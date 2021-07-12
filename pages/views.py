122d2712164d6fc23ecf7524e7a2bc9091aaac3e
Inte-H <sjobs66@gmail.com>
2021-07-12 22:13:50 +0900

    intro, about me 페이지

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def landing(request):
+    return render(
+        request,
+        'page/landing.html',
+    )
+
+
+def about_me(request):
+    return render(
+        request,
+        'page/about_me.html',
+    )
