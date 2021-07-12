4d6a728e9f6c526a47b0d96398cd51a07d39745c
Inte-H <sjobs66@gmail.com>
2021-07-12 21:48:19 +0900

    FBV 블로그 상세페이지

--- blog/views.py
+++ blog/views.py
@@ -12,3 +12,14 @@ def index(request):
             'posts': posts,
         }
     )
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blog/post_detail.html',
+        {
+            'post' : post
+        }
+    )
