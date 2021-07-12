34744f961a73c96814f7be83ba495ba2d7ed6b53
Inte-H <sjobs66@gmail.com>
2021-07-12 22:35:05 +0900

    CBV 블로그 상세페이지

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -7,6 +7,9 @@ class PostList(ListView):
     model = Post
     ordering = '-pk'
 
+
+class PostDetail(DetailView):
+    model = Post
 
 # def index(request):
 #     posts = Post.objects.all().order_by('-pk')
@@ -19,13 +22,13 @@ class PostList(ListView):
 #         }
 #     )
 
-def post_detail(request, pk):
-    post = Post.objects.get(pk=pk)
-
-    return render(
-        request,
-        'blog/post_detail.html',
-        {
-            'post' : post
-        }
-    )
+# def post_detail(request, pk):
+#     post = Post.objects.get(pk=pk)
+#
+#     return render(
+#         request,
+#         'blog/post_detail.html',
+#         {
+#             'post' : post
+#         }
+#     )
